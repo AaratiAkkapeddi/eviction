39cd27cc2d1124b933752630f598c84f3b5909a8
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2020-08-10 10:39:17 -0400

    adds pass functionality and performer cards

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -26,9 +26,10 @@ class About extends Component {
 	  }
       componentDidUpdate(){
 var linkList = document.getElementsByClassName('about')[0];
-    var otherlinks = linkList.getElementsByTagName('a')
+    
 
-    if(otherlinks){
+    if(linkList){
+      var otherlinks = linkList.getElementsByTagName('a')
     for (var i = 0; i < otherlinks.length; i++) {
 
          otherlinks[i].target = "_blank";
